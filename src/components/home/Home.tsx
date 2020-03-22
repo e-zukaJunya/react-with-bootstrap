a0e2093c00954e24c85906024879984c96696f77
Junya Iizuka <minazukitombo@gmail.com>
2020-03-23 01:35:34 +0900

    tab layout

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,7 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
+import { Button, Tabs, Tab } from "react-bootstrap";
 
 const Home = () => {
-  return <div>ホーム</div>;
+  const [key, setKey] = useState("home");
+  return (
+    <div>
+      <Tabs id="controlled-tab-example" activeKey={key} onSelect={(k: string) => setKey(k)}>
+        <Tab eventKey="home" title="Home">
+          <div>1個目</div>
+        </Tab>
+        <Tab eventKey="profile" title="Profile">
+          <Button>bst button</Button>
+        </Tab>
+        <Tab eventKey="contact" title="Contact" disabled>
+          <div>3個目</div>
+        </Tab>
+      </Tabs>
+    </div>
+  );
 };
 
 export default Home;
